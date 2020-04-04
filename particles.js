6113c85e9f8cf3e8081ba9602821ed09d6d95819
Daniil <ddan62626@gmail.com>
2020-04-04 20:51:05 +0300

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,250 @@
+
+class Point{
+    constructor(_x,_y,_angle,_speed){
+        if(!Point._id){
+            Point._id = 0;
+        }
+        this.id = Point._id++;
+        this.x = _x;
+        this.y = _y;
+        this.angle = _angle;
+        this.speed = _speed;
+        this.time = performance.now();
+        this.radius = Random(2,4);
+    }
+}
+
+
+const Random = (start,end) => {
+    return start + Math.random()*(end - start);
+}
+
+const fill = (r,g,b,a = 1) => {
+    let ctx = Canvas.context;
+    ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
+}
+
+const stroke = (r,g,b,a = 1) => {
+    let ctx = Canvas.context;
+    ctx.strokeStyle = `rgba(${r},${g},${b},${a})`;
+}
+
+const strokeWidth = (w) => {
+    let ctx = Canvas.context;
+    ctx.lineWidth = w;
+}
+
+const circle = (x,y,r) => {
+    let ctx = Canvas.context;
+    ctx.beginPath();
+    ctx.arc(x,y,r,0,2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
+
+const line = (x1,y1,x2,y2) =>{
+    let ctx = Canvas.context;
+    ctx.beginPath();
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.stroke();
+}
+
+
+function fps(){
+    return 1./Time.deltaTime;
+}
+
+
+
+const Points = {};
+const Canvas = {};
+const MaxPoints = 100;
+const Speed = 100;
+const EstimatedFPS = 30;
+const MaxDist = 160;
+const Time = {
+    deltaTime:1./EstimatedFPS
+}
+
+
+function createCanvas(_width,_height){
+    Canvas.width = _width;
+    Canvas.height = _height;
+    Canvas.canvas = document.createElement('canvas');
+    document.body.append(Canvas.canvas);
+    Canvas.canvas.width = _width;
+    Canvas.canvas.height = _height;
+    Canvas.context = Canvas.canvas.getContext("2d");
+}
+
+
+function resizeCanvas(_width,_height){
+    Canvas.width = _width;
+    Canvas.height = _height;
+    Canvas.canvas.width = _width;
+    Canvas.canvas.height = _height;
+}
+
+function generatePoint(){
+    let edge = Math.floor(Math.random()*4);
+
+    /**
+     * 0 - top
+     * 1 - right
+     * 2 - bottom
+     * 3 - left
+     */
+
+    let x,y; 
+    let p;
+    if(edge == 0){
+        y = -10;
+        x = Math.random()*Canvas.width;
+        p = new Point(x,y,Random(Math.PI,2*Math.PI),Random(Speed - Speed*0.2,Speed + Speed*0.2))
+    }else if(edge == 1){
+        x = Canvas.width + 10;
+        y = Math.random()*Canvas.height;
+        p = new Point(x,y,Random(Math.PI*0.5,1.5*Math.PI),Random(Speed - Speed*0.2,Speed + Speed*0.2))
+    }else if(edge == 2){
+        y = Canvas.height + 10;
+        x = Math.random()*Canvas.width;
+        p = new Point(x,y,Random(0,Math.PI),Random(Speed - Speed*0.2,Speed + Speed*0.2))
+    }else{
+        x = -10;
+        y = Math.random()*Canvas.height;
+        p = new Point(x,y,Random(-0.5*Math.PI,0.5*Math.PI),Random(Speed - Speed*0.2,Speed + Speed*0.2))
+    }
+    
+    Points[p.id] = p;
+}
+
+
+
+
+function setup(){    
+    createCanvas(window.innerWidth,window.innerHeight);
+    Canvas.canvas.style.backgroundImage = `url(https://picsum.photos/${Canvas.width}/${Canvas.height})`;
+    for(let i = 0; i < MaxPoints;++i){
+        generatePoint();
+    }
+
+
+    for(let i in Points){
+        let p = Points[i];
+        p.x = Random(0,Canvas.width);
+        p.y = Random(0,Canvas.height);
+    }
+}
+
+
+setup();
+
+function draw(){
+    if(!Canvas.canvas) return;
+    let ctx = Canvas.context;
+    
+    stroke(255,255,255,255);
+
+    ctx.clearRect(0,0,Canvas.width,Canvas.height);
+    let point;
+    let idToDelete = [];
+    for(let i in Points){
+        
+        point = Points[i];
+        if(performance.now() - point.time > 1000 && (point.x > Canvas.width+10 || point.x < -10 || point.y > Canvas.height + 10 || point.y < -10)){
+            idToDelete.push(i);
+            
+        }
+
+        fill(255,255,255,255);
+        strokeWidth(0);
+        //strokeWidth(point.radius*1);
+        circle(point.x,point.y,point.radius);
+
+ 
+        point.x += Math.cos(point.angle)*Time.deltaTime*point.speed;
+        point.y -= Math.sin(point.angle)*Time.deltaTime*point.speed;
+
+        let k = false;
+        for(let j in Points){
+            //if(k >= 15) continue;
+            if(i == j){
+                k = true;
+                continue;
+            }
+            if(!k) continue;
+            
+            let a = Points[j];
+            let d = Math.sqrt((point.x - a.x)*(point.x - a.x) + (point.y - a.y)*(point.y - a.y));
+            if(d >= MaxDist) continue;
+
+            
+            strokeWidth((1.-d/MaxDist) * 1.5);
+            line(a.x,a.y,point.x,point.y);
+        }
+
+
+    }
+
+    for(let i of idToDelete){
+        delete Points[i];
+    }
+
+
+    for(let i = Object.keys(Points).length; i < MaxPoints; ++i){
+        generatePoint();
+    }
+
+}
+
+
+
+window.onresize = () =>{
+    resizeCanvas(window.innerWidth,window.innerHeight);
+    Canvas.canvas.style.backgroundImage = `url(https://picsum.photos/${Canvas.width}/${Canvas.height})`;
+}
+
+
+
+
+
+
+
+ function interval(duration, fn){
+    this.baseline = undefined
+    
+    this.run = function(){
+      if(this.baseline === undefined){
+        this.baseline = performance.now()
+      }
+      fn()
+      var end = performance.now()
+      this.baseline += duration
+   
+      var nextTick = duration - (end - this.baseline)
+      if(nextTick<0){
+        nextTick = 0
+      }
+      (function(i){
+          i.timer = setTimeout(function(){
+          i.run(end)
+        }, nextTick)
+      }(this))
+    }
+  
+  this.stop = function(){
+     clearTimeout(this.timer)
+   }
+  }
+
+
+  let last = performance.now();
+  var MainLoop = new interval(1000./EstimatedFPS, function(){
+    Time.deltaTime = (performance.now() - last)*0.001;
+    last = performance.now();
+    draw();
+
+
+  })
+  MainLoop.run()
